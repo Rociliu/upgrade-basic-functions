fe6ac6d7685647ea49263bb43fee8308ab0a8fa7
Rociliu <mrrosa.carrasco@gmail.com>
2022-12-11 18:48:03 +0100

    Iteracion#4

--- Iteracion4.js
+++ Iteracion4.js
@@ -2,16 +2,15 @@
 //Calcular un promedio 
 
 const numbers = [12, 21, 38, 5, 45, 37, 6];
-function average() {
-  let sum = 0;
+function average(param) {
+  let suma = 0;
   for (let i=0; i<numbers.length; i++){
-    sum = sum + numbers[i];
+    suma = suma + numbers[i];
   }
 
-  let promedio = sum / numbers.length; 
+  let promedio = suma / numbers.length; 
   return promedio;
 }
- let media = average(numbers);
-
- console.log("El promedio es de: " + media); 
+ let result = average(numbers);
+ console.log("El promedio es de: " + result); 
 
